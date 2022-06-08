37331afd555ceab2d205c0a32b7e5554d433626d
Girolamo <girolamosciascia@gmail.com>
2022-06-08 14:24:53 +0200

    Aggiunti file e fatto il commit

---
+++ esercitazioneGit/src/esercitazioneGit/Git.java
@@ -0,0 +1,13 @@
+package esercitazioneGit;
+
+public class Git {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello world");
+		System.out.println("hello");
+		System.out.println("hello");
+		
+	}
+
+}
---
+++ esercitazioneGit/src/esercitazioneGit/Prova.java
@@ -0,0 +1,5 @@
+package esercitazioneGit;
+
+public class Prova {
+
+}
